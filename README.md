27c7eeb98b945cecbc66e20971f827fe92dcfbc2
Christian Adell <chadell@gmail.com>
2018-08-26 19:22:49 +0200

    Basic gissip

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# gissip
-Git gossip
+# GISSIP: Git Gossip 
+
+Have you ever wondered if any change has happened to one of your loved GitHub repos?
+
+This simple tool give you a simple way to track them and get links to the merge commits that have happened on the last X days.
+
+Some GitHub APIs need authorization (others as the public GitHub.com impose rate limit, so you can authenticate yourself)
+To create a GitHub token do:
+* Your profile -> Settings -> Developer Settings -> Personal Access Tokens (Repo scope is enough)
+
+```bash
+$ python -m gissip.main  -h
+usage: main.py [-h] [-v] [-a API] [-o OWNER] [-r REPO] [-d DAYS] [-y YAML]
+               [-t TOKEN]
+
+Get merge commits of your favourite repos
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         increase output verbosity
+  -a API, --api API     Github API URL
+  -o OWNER, --owner OWNER
+                        Github owner
+  -r REPO, --repo REPO  Repository you are checking
+  -d DAYS, --days DAYS  How many days you want to observe
+  -y YAML, --yaml YAML  Path to YAML file where you define you loved repos
+  -t TOKEN, --token TOKEN
+                        Github Access Token to get better rate access to API
+```
+
+## Examples
+
+$ python -m gissip.main  -d 7 -y my_loved_repos.yml
+$ python -m gissip.main  -d 7 -y my_loved_repos.yml -t xxx -a "my own GitHub API"
+$ python -m gissip.main -o chadell -r gissip
+
+### YAML file
+
+You can also define a YAML file containing several repos from several owners, follow the below format and pass to the script:
+```yaml
+---
+- chadell:
+  - chadell.github.io
+  - gissip
+
+- dachad:
+  - tcpgoon
+```
+
+## TODO
+
+* Testing, CI, CD
+* Upload PIP packages
---
+++ gissip/main.py
@@ -0,0 +1,129 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+import argparse
+import requests
+from datetime import datetime, timedelta
+import pprint
+import yaml
+
+log = logging.getLogger('critical_peering')
+out_hdlr = logging.StreamHandler(sys.stdout)
+out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+out_hdlr.setLevel(logging.INFO)
+log.addHandler(out_hdlr)
+log.setLevel(logging.INFO)
+
+
+def bordered(text):
+    lines = text.splitlines()
+    width = max(len(s) for s in lines)
+    res = ['┌' + '─' * width + '┐']
+    for s in lines:
+        res.append('│' + (s + ' ' * width)[:width] + '│')
+    res.append('└' + '─' * width + '┘')
+    return '\n'.join(res)
+
+
+def get_commits_within_window(api, headers, owner, repo, days):
+    req_response = requests.get(api + "/repos/{}/{}/commits".format(owner, repo), headers=headers)
+
+    if req_response.status_code == 404:
+        raise ValueError("Owner or Repo not found")
+    elif req_response.status_code == 403:
+        raise ValueError("Rate Limit error: {}. Current value of access per hour: {}".format(req_response.json()['message'], req_response.headers['X-RateLimit-Limit']))
+    elif req_response.status_code == 401:
+        raise ValueError("Failed Authorization")        
+
+    result_commits = []
+    all_commits = req_response.json()
+    for commit in all_commits:
+        commit_date =  datetime.strptime(commit['commit']['author']['date'] ,'%Y-%m-%dT%H:%M:%SZ')
+        if commit_date > datetime.now() - timedelta(days=days):
+            result_commits.append(commit)
+    
+    return result_commits
+
+
+def print_results(results):
+    for owner in results:
+        for repo in results[owner]:
+            print(bordered("Commits submitted for {}/{}\n".format(owner, repo)))
+            for commit in results[owner][repo]:
+                print commit['html_url']
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Get merge commits of your favourite repos')
+
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument('-a', '--api', help='Github API URL', default='https://api.github.com')
+    parser.add_argument('-o', '--owner', help='Github owner')
+    parser.add_argument('-r', '--repo', help='Repository you are checking')
+    parser.add_argument('-d', '--days', help='How many days you want to observe', type=int, default=1)
+    parser.add_argument('-y', '--yaml', help='Path to YAML file where you define you loved repos')
+    parser.add_argument('-t', '--token', help='Github Access Token to get better rate access to API')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    results = {}
+
+    if args.token:
+        headers = {'Authorization': 'token ' + args.token}
+        req_response = requests.get(args.api, headers=headers)
+        if req_response.status_code == 401:
+            log.info("Authorization error: {}".format(req_response.json()['message']))
+        else:
+            log.info ("INFO: Successfully authenticated")
+    else:
+        headers = None
+
+    if args.yaml:
+        with open(args.yaml, 'r') as yaml_file:
+            owners_and_repos = yaml.load(yaml_file)
+
+        for owner_and_repos in owners_and_repos:
+            owner = owner_and_repos.keys()[0]
+            for repos in owner_and_repos.values():
+                results[owner] = {}
+                for repo in repos:
+                    try:
+                        if repo in results[owner].keys():
+                            results[owner][repo].add(get_commits_within_window(args.api, headers, owner, repo, args.days))
+                        else:
+                            results[owner][repo] = get_commits_within_window(args.api, headers, owner, repo, args.days)
+                    except ValueError as e:
+                        log.error("{}, Owner: {}, Repo: {}".format(e, owner, repo))
+    elif args.repo and args.owner:
+        try:
+            results[args.owner] = get_commits_within_window(args.api, headers, args.owner, args.repo, args.days)
+        except ValueError as e:
+            log.error("{}, Owner: {}, Repo: {}".format(e, args.owner, args.repo))
+    else:
+        print "pass me something"
+
+    # pprint.pprint(results)
+    print_results(results)
+
+
+if __name__ == "__main__":
+
+    try:
+        main()
+        sys.exit(0)
+    except Exception as e:
+        logging.critical(
+            str(e),
+            exc_info=True
+        )
+        sys.exit(1)
