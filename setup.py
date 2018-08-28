b4c580652907738e24b55277f96f1975dc41f9e7
Christian Adell <chadell@gmail.com>
2018-08-28 09:30:21 +0200

    Back to python3.6

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
 
         "Topic :: Internet :: WWW/HTTP",
     ],
